c9adb2dfe1c86b86ca1b0e5b064ba795b6c20041
zhengzhiren <zhengzhiren@gmail.com>
2014-10-21 09:48:24 +0800

    "update" and "commit" command for the script

--- vimrc.sh
+++ vimrc.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 THIS_DIR=$(dirname $(readlink -f $0))
+cmd=$1
 
-echo Committing \"$HOME/.vimrc\" to git.
+function update()
+{
+	echo Updating \"$HOME/.vimrc\"
+	cd $THIS_DIR; git pull
+	cp $HOME/.vimrc $HOME/.vimrc.bak #make a backup
+	cp $THIS_DIR/.vimrc $HOME/.vimrc
+}
 
- 
-cp $HOME/.vimrc $THIS_DIR/
-cd $THIS_DIR; git commit .vimrc; git push
+function commit()
+{
+	echo Committing \"$HOME/.vimrc\" to git.
+	cp $HOME/.vimrc $THIS_DIR/
+	cd $THIS_DIR; git commit .vimrc; git push
+}
+
+case $cmd in
+	update)
+	update
+	;;
+	commit)
+	commit
+	;;	
+	*)
+	echo $"Usage: $0 {update|commit}"
+esac
